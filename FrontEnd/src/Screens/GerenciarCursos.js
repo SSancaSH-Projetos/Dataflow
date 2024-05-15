d7a1d3763d494cd6033bf867bf5176be06af9608
vanessabt86 <vanessa_bt@hotmail.com>
2024-05-15 17:25:20 -0300

    continuar tela gerenciamento curso

--- FrontEnd/src/Screens/GerenciarCursos.js
+++ FrontEnd/src/Screens/GerenciarCursos.js
@@ -1,17 +1,61 @@
 import React, { useState, useEffect } from "react";
+import { View, StyleSheet, Text, TouchableOpacity, Picker } from "react-native";
 import {
-  View,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  TextInput,
-  Picker, // Import Picker from react-native
-} from "react-native";
+  TextField,
+  Typography,
+  Breadcrumbs,
+  Link,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Button,
+  InputLabel,
+  MenuItem,
+  Select,
+} from "@mui/material";
 import axios from "axios";
-import TemplateCrud from "../Componentes/TemplateCrud";
-import { Typography } from "@mui/material";
+import TemplateCrud from "../Components/TemplateCrud";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
 
-const AdicionarTurmas2 = ({ navigation }) => {
+const GerenciarCursos = ({ navigation }) => {
+  const [nome, setNome] = useState (""); 
+  const [cargaHoraria, setCargaHoraria] = useState("");
+  const [nivel, setNivel] = useState("");
+  const [cursos, setCursos] =useState([]);
+
+
+  const fetchCursos = async () => {
+    try {
+      const response = await axios.get("http://localhost:8080/curso");
+      setCursos(response.data);
+    } catch (error) {
+      console.error("Erro ao obter cursos:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCursos();
+  }, []);
+
+  const handleAddCurso = async () => {
+    try {
+      const response = await axios.post("http://localhost:8080/curso", {
+        nome: nome,
+        cargaHoraria: cargaHoraria,
+        nivel: nivel
+      });
+      console.log(response.data);
+
+      fetchCursos();
+    } catch (error) {
+      console.error("Erro ao adicionar curso:", error);
+    }
+  };
   
 
   return (
@@ -23,4 +67,4 @@ const AdicionarTurmas2 = ({ navigation }) => {
 
 const styles = StyleSheet.create({});
 
-export default AdicionarTurmas2;
+export default GerenciarCursos;
