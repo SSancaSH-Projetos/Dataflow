34bb2732af3e040aeb0153e4abc32bc5390e428b
Leonardo <158601505+LeoNogueira1@users.noreply.github.com>
2024-05-22 17:22:19 -0300

    Update GerenciarAvaliacao.js

--- FrontEnd/src/Screens/GerenciarAvaliacao.js
+++ FrontEnd/src/Screens/GerenciarAvaliacao.js
@@ -1,25 +1,18 @@
 import React, { useState, useEffect } from "react";
-import { View, StyleSheet, Text, TouchableOpacity, Picker, ScrollView } from "react-native";
+import { View, StyleSheet, ScrollView } from "react-native";
 import {
-  TextField,
   Typography,
-  Breadcrumbs,
-  Link,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Paper,
-  Button,
-  InputLabel,
   MenuItem,
   Select,
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  Paper,
 } from "@mui/material";
 import axios from "axios";
 import TemplateCrud from "../Components/TemplateCrud";
-
 
 const GerenciarAvaliacao = ({ navigation }) => {
   const [curso, setCursoId] = useState("");
@@ -28,9 +21,11 @@ const GerenciarAvaliacao = ({ navigation }) => {
   const [turmas, setTurmas] = useState([]);
   const [ucId, setUcId] = useState([]);
   const [ucs, setUCs] = useState([]);
+  const [saId, setSaId] = useState("");
+  const [sass, setSas] = useState([]);
+  const [capacidadeId, setCapacidadeId] = useState("");
+  const [capacidades, setCapacidades] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-
-
 
   const fetchCursos = async () => {
     try {
@@ -59,10 +54,35 @@ const GerenciarAvaliacao = ({ navigation }) => {
     }
   };
 
+  const fetchCapacidade = async () => {
+    try {
+      const response = await axios.get("http://localhost:8080/capacidade");
+      setCapacidades(response.data);
+    } catch (error) {
+      console.error("Erro ao obter capacidade", error);
+    }
+  };
+
+  const fetchSa = async () => {
+    try {
+      const response = await axios.get("http://localhost:8080/sa");
+      setSas(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.error("Erro ao obter situação de aprendizagem:", error);
+    }
+  };
+
   useEffect(() => {
     const fetchData = async () => {
       setIsLoading(true);
-      await Promise.all([fetchCursos(), fetchTurmas(), fetchUCs()]);
+      await Promise.all([
+        fetchCursos(),
+        fetchTurmas(),
+        fetchUCs(),
+        fetchCapacidade(),
+        fetchSa(),
+      ]);
       setIsLoading(false);
     };
     fetchData();
@@ -70,100 +90,129 @@ const GerenciarAvaliacao = ({ navigation }) => {
 
   return (
     <ScrollView>
-    <TemplateCrud>
-      <View style={styles.mainContainer}>
-        <View style={styles.breadcrumbsContainer}>
-        </View>
-        <View style={styles.contentContainer}>
-          <View style={styles.formContainer}>
-            <Typography
-              variant="h6"
-              noWrap
-              component="div"
-              style={styles.title}>
-              Gerenciar Avaliação
-            </Typography>
+      <TemplateCrud>
+        <View style={styles.mainContainer}>
+          <View style={styles.contentContainer}>
+            <View style={styles.formContainer}>
+              <Typography
+                variant="h6"
+                noWrap
+                component="div"
+                style={styles.title}
+              >
+                Gerenciar Avaliação
+              </Typography>
 
-            <Select
-              labelId="aluno-select-curso"
-              id="curso-select"
-              value={curso}
-              onChange={(e) => setCursoId(e.target.value)}
-              sx={{ marginBottom: "20px" }}
-              displayEmpty
-            >
-              <MenuItem value="" disabled>
-                Selecionar Curso
-              </MenuItem>
-              {cursos.map((curso) => (
-                <MenuItem key={curso.id} value={curso.id}>
-                  {curso.nome}
+              <Select
+                labelId="curso-select-label"
+                id="curso-select"
+                value={curso}
+                onChange={(e) => setCursoId(e.target.value)}
+                sx={{ marginBottom: "20px" }}
+                displayEmpty
+              >
+                <MenuItem value="" disabled>
+                  Selecionar Curso
                 </MenuItem>
-              ))}
-            </Select>
+                {cursos.map((curso) => (
+                  <MenuItem key={curso.id} value={curso.id}>
+                    {curso.nome}
+                  </MenuItem>
+                ))}
+              </Select>
 
-
-            <Select
-              labelId="turma-select-label"
-              id="turma-select"
-              value={turmaId}
-              onChange={(e) => setTurmaId(e.target.value)}
-              sx={{ marginBottom: "20px" }}
-              displayEmpty
-            >
-              <MenuItem value="" disabled>
-                Selecionar Turma
-              </MenuItem>
-              {turmas.map((turma) => (
-                <MenuItem key={turma.id} value={turma.id}>
-                  {turma.sigla}
+              <Select
+                labelId="turma-select-label"
+                id="turma-select"
+                value={turmaId}
+                onChange={(e) => setTurmaId(e.target.value)}
+                sx={{ marginBottom: "20px" }}
+                displayEmpty
+              >
+                <MenuItem value="" disabled>
+                  Selecionar Turma
                 </MenuItem>
-              ))}
-            </Select>
+                {turmas.map((turma) => (
+                  <MenuItem key={turma.id} value={turma.id}>
+                    {turma.sigla}
+                  </MenuItem>
+                ))}
+              </Select>
 
-
-            <Select
-              labelId="uc-select-label"
-              id="uc-select"
-              value={ucId}
-              onChange={(e) => setUcId(e.target.value)}
-              sx={{ marginBottom: "20px" }}
-              displayEmpty
-            >
-              <MenuItem value="" disabled>
-                Selecionar UC
-              </MenuItem>
-              {ucs.map((uc) => (
-                <MenuItem key={uc.id} value={uc.id}>
-                  {uc.sigla}
+              <Select
+                labelId="uc-select-label"
+                id="uc-select"
+                value={ucId}
+                onChange={(e) => setUcId(e.target.value)}
+                sx={{ marginBottom: "20px" }}
+                displayEmpty
+              >
+                <MenuItem value="" disabled>
+                  Selecionar UC
                 </MenuItem>
-              ))}
-            </Select>
+                {ucs.map((uc) => (
+                  <MenuItem key={uc.id} value={uc.id}>
+                    {uc.sigla}
+                  </MenuItem>
+                ))}
+              </Select>
 
+              <Select
+                labelId="sa-select-label"
+                id="sa-select"
+                value={saId}
+                onChange={(e) => setSaId(e.target.value)}
+                sx={{ marginBottom: "20px" }}
+                displayEmpty
+              >
+                <MenuItem value="" disabled>
+                  Selecionar SA
+                </MenuItem>
+                {sass.map((sa) => (
+                  <MenuItem key={sa.id} value={sa.id}>
+                    {sa.descricao}
+                  </MenuItem>
+                ))}
+              </Select>
 
+              <Select
+                labelId="capacidade-select-label"
+                id="capacidade-select"
+                value={capacidadeId}
+                onChange={(e) => setCapacidadeId(e.target.value)} // Corrigido para chamar setCapacidade
+                sx={{ marginBottom: "20px" }}
+                displayEmpty
+              >
+                <MenuItem value="" disabled>
+                  Selecionar Capacidades
+                </MenuItem>
+                {capacidades.map((capacidade) => (
+                  <MenuItem key={capacidade.id} value={capacidade.id}>
+                    {capacidade.descricao}
+                  </MenuItem>
+                ))}
+              </Select>
+            </View>
+            <View style={styles.tableContainer}>
+              <TableContainer component={Paper}>
+                <Table>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Nome</TableCell>
+                      <TableCell>Nível</TableCell>
+                      <TableCell>Carga Horária</TableCell>
+                      <TableCell>Ação</TableCell>
+                    </TableRow>
+                  </TableHead>
+                </Table>
+              </TableContainer>
+            </View>
           </View>
-          <View style={styles.tableContainer}>
-            <TableContainer component={Paper}>
-              <Table>
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Nome</TableCell>
-                    <TableCell>Nivel</TableCell>
-                    <TableCell>Carga Horaria</TableCell>
-                    <TableCell>Ação</TableCell>
-                    
-                  </TableRow>
-                </TableHead>
-              </Table>
-            </TableContainer>
-          </View>
         </View>
-      </View>
-    </TemplateCrud>
+      </TemplateCrud>
     </ScrollView>
   );
 };
-
 const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
@@ -195,6 +244,5 @@ const styles = StyleSheet.create({
   },
 });
 
-
-
 export default GerenciarAvaliacao;
+
